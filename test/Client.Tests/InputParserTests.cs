dc8980334a9a0f07d2964008b0e701f1e16c45a5
Jeff Ogata <jeffogata@outlook.com>
2021-05-02 08:36:17 -0500

    Set a coverage threshold and add tests to meet it

--- test/Client.Tests/InputParserTests.cs
+++ test/Client.Tests/InputParserTests.cs
@@ -1,5 +1,6 @@
 ﻿namespace Client.Tests
 {
+    using System;
     using Xunit;
 
     public class InputParserTests
@@ -11,6 +12,21 @@
             var parser = new InputParser();
             var input = "add 123 456";
             var expected = new CalculatorInput(Operation.Add, 123, 456);
+
+            // act
+            var actual = parser.ParseInput(input);
+
+            // assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ParseInputWithSubtractInputParsesToExpectedResult()
+        {
+            // arrange
+            var parser = new InputParser();
+            var input = "sub 25 10";
+            var expected = new CalculatorInput(Operation.Subtract, 25, 10);
 
             // act
             var actual = parser.ParseInput(input);
@@ -32,6 +48,19 @@
 
             // assert
             Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ParseInputWithEmptyStringThrowsArgumentException()
+        {
+            // arrange
+            var parser = new InputParser();
+
+            // act
+            var exception = Assert.Throws<ArgumentException>(() => parser.ParseInput(string.Empty));
+
+            // assert
+            Assert.NotNull(exception);
         }
     }
 }
